6f1de876e963399a6f19b38b4b2c6870ee29cedb
Vinicius V. Parizoto <vini.parizoto@live.com>
2024-11-29 08:17:06 -0300

    feat: criação do rodapé e arquivo excalidraw #main

--- src/components/template/Page.svelte
+++ src/components/template/Page.svelte
@@ -1,10 +1,12 @@
 <script>
+  import Footer from "./Footer.svelte";
   import Header from "./Header.svelte";
 </script>
 
 <div class="flex flex-col min-h-screen bg-black text-white">
   <Header />
-  <main>
+  <main class="flex-1 max-w-screen-xl justify-center">
     <slot />
   </main>
+  <Footer />
 </div>
